6ed1b19c4f0e41f941c1a7c13b62234cadc04c4e
Felipe Marques <felipeonmarques@gmail.com>
2024-07-16 16:47:13 -0300

    Resolvendo exercícios ConjuntoConviadados, CadastroProdutos e AgendaContatos
    Signed-off-by: Felipe Marques <felipeonmarques@gmail.com>

---
+++ src/main/java/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,42 @@
+package main.java.set.Ordenacao;
+
+import java.util.*;
+
+public class CadastroProdutos {
+
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos(){
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade){
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome(){
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreco(){
+        Set<Produto> produtosPorPreco = new TreeSet<>(new Produto.ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1l, "produto1", 15, 5);
+        cadastroProdutos.adicionarProduto(2l, "produto8", 8, 10);
+        cadastroProdutos.adicionarProduto(1l, "produto3", 3, 2);
+        cadastroProdutos.adicionarProduto(9l, "produto9", 9, 2);
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+}
