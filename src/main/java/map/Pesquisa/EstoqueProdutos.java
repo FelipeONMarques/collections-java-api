b52edfdd90e242baf6f517b2616022a2a508f264
Felipe Marques <felipeonmarques@gmail.com>
2024-07-16 22:58:37 -0300

    Resolvendo exercícios Map
    Signed-off-by: Felipe Marques <felipeonmarques@gmail.com>

---
+++ src/main/java/map/Pesquisa/EstoqueProdutos.java
@@ -0,0 +1,60 @@
+package main.java.map.Pesquisa;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class EstoqueProdutos {
+
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos(){
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calculaValorTotal(){
+        double valorTotalEstoque = 0d;
+        if (!estoqueProdutosMap.isEmpty()){
+            for (Produto p : estoqueProdutosMap.values()){
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (!estoqueProdutosMap.isEmpty()){
+            for (Produto p : estoqueProdutosMap.values()){
+                if (p.getPreco() > maiorPreco){
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.exibirProdutos();
+
+        estoque.adicionarProduto(1L, "Produto A", 10, 5.0);
+        estoque.adicionarProduto(2L, "Produto B", 5, 10.0);
+        estoque.adicionarProduto(3L, "Produto C", 2, 15.0);
+        estoque.adicionarProduto(3L, "Produto D", 2, 25.0);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor do estoque: R$" + estoque.calculaValorTotal());
+        System.out.println("Produto mais caro: " + estoque.obterProdutoMaisCaro());
+    }
+}
